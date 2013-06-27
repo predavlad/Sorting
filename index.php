244417bcaac1aafb8348dbe7e90289698a5c3ae6
predavlad <preda.vlad@yahoo.com>
2013-06-27 10:26:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * These is something I did in my spare time to remember the sorting alhorithms out there
+ * The default PHP sort (that uses quicksort behind the scenes) is much faster than anything
+ * you can write in pure PHP. Even with optimizations and opcode cache (which I didn't do / take
+ * into consideration), these scrips are orders of magnitude slower than PHP sort() - which is used
+ * in PhpDefaultSort class.
+ */ 
 include "includes/autoload.php";
 include "includes/app.php";
 include "includes/UnitTester.php";
@@ -20,4 +26,4 @@ $algorithmTester->run();
 
 echo '<br>';
 echo '========================';
-echo '<br><br>Script ended';
+echo '<br>Script ended';
